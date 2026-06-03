e801858d3f41f6bcb36b339b2e7b395af42d18f5
dipakarki9 <dipakarki766@gmail.com>
2026-06-03 19:01:59 +0545

    first commit

---
+++ first.js
@@ -0,0 +1,93 @@
+
+// const student = {
+//     name: "garima",
+//     addres: "jaypur",
+//     age: 20,
+//     isPass: true,
+// };
+// console.log(student.name);
+
+
+/* logical operators
+or operators */
+// let a = 5;
+// let b = 6;
+// console.log("cond1 || cond2 = " ,a < b || a==b);
+
+/* conditional statements*/
+//  let age=25;
+//  if(age>18){
+//     console.log("you can vote");
+//  }
+
+// alert("hello!");
+//  let num=prompt("enter a number");
+//  if(num%5==0)
+//  {
+//    console.log( num,"number is multiple of 5");
+//  }
+//    else {
+//       console.log(num,"not multiple of 5");
+//    }
+ 
+// for loop
+// for(let count=1; count<=10; count++){
+//   console.log("Hello my name is garima")
+// }
+// let age=18;
+// if(age>=18){
+//   console.log("you can vote");
+// }
+
+/*if else statemenet
+*/
+// let age=10;
+// if(age>=18){
+//   console.log("you are elligibleto vote");
+// }
+//   else {
+//     console.log("you are not elligible to vote");
+//   }
+  
+/*Types of operators
+1. Arithmetic operators(+,-,*,/,%,++,--)
+Example:
+*/
+var a=10;
+var b=5;
+document.write(a+b);
+/*
+2.Assignment operators(=,+=,-=,*=,/=,%=):
+Example:
+*/
+var x=10;
+x+=5;
+document.write(x);
+/* 
+3.Comparison Operators(==,===,!=,!==,>,<,>=,<=):
+example:
+*/
+var a=10;
+var b=5;
+document.write(a>b);
+
+/*
+4.Logical operators(&&,||,!):
+Example:
+*/
+var a =10;
+var b=5;
+document.write(a>5&&b<10);
+/*
+5.Conditional Operators(conditional?expression1:expression2):
+Example:
+*/
+var age=18;
+var result=(age>=18)?"Eligible":"Not Elligible";
+document.write(result);
+
+/*
+6.String Operator:It is used to combine or concatenate two or more string into a single string.
+Example:
+*/
+document.write("hello"+"world");
