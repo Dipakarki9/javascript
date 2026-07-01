f20b25e8ca64415d247ba58594410216b6feb1f4
dipakarki9 <dipakarki766@gmail.com>
2026-07-01 17:49:33 +0545

    events

--- practice/object/DOM/Eventhandling/event.js
+++ practice/object/DOM/Eventhandling/event.js
@@ -1,4 +1,69 @@
-let button = document.getElementById("btn");
-button.addEventListener("click",function(){
-    alert("button clicked!");
+// let button = document.getElementById("btn");
+// button.addEventListener("clickw",function(){
+//     alert("button clicked!");
+// })
+
+// let countt = document.querySelector("#btn");
+
+// let count = 0;
+// countt.addEventListener("click",() =>{
+//     count++;
+//  let mssg = 'the button has been clicked';
+//  console.log(mssg);
+// })
+
+// click
+// let btnn = document.getElementById("btn");
+// btnn.addEventListener("click",() => {
+//     console.log("button clicked");
+// })
+// dbclick
+// let btnn = document.getElementById("btn");
+// btnn.addEventListener("dblclick",() => {
+//     // console.log("button clicked");
+// })
+// mousemove
+
+ /*let mouseMove = document.getElementById("btn");
+mouseMove.addEventListener("mousemove",() =>{
+    console.log("Mouse moving");
 })
+    */
+
+// mouseover
+/*
+let mouseOver = document.getElementById("btn");
+mouseOver.addEventListener("mouseover",() =>{
+    console.log("mouse entered")
+})
+*/
+
+
+/*
+let mouseOut = document.getElementById("btn");
+mouseOut.addEventListener("mouseout",() =>{
+    console.log("mouse entered")
+})
+    */
+
+/*
+
+document.addEventListener("DOMContentLoaded",() =>{
+     console.log("mouse entered");
+ })
+*/
+
+
+/*
+document.addEventListener("keydown",(event) =>{
+     console.log("mouse entered");
+});
+*/
+/*document.addEventListener("keyup",(event) =>{
+  console.log("key released");
+ })
+  */
+ /*document.addEventListener("scroll",() =>{
+    console.log("scrolling");
+});
+*/
